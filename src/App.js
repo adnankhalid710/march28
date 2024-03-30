dcf4ea5324536901032de38ad4e79c2235a7f0e8
Muhammad Adnan Khalid <adnankhl@yahoo.com>
2024-03-31 05:11:20 +0800

    insert image

--- src/App.js
+++ src/App.js
@@ -4,22 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Wednesday, March 28, 2024.Wednesday.
-          
-         
-        </p>
-        <a
-          className="App-link"
-          href="https://www.researchgate.net/profile/Sadaf-Habib"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sadaf Habib
-        </a>
-      </header>
-      <img src="ginko.jpg" alt="Dinosaur" />
+    <h1> In the name of Allah</h1>
+    <img src="ginko.jpg" />
     </div>
   );
 }
