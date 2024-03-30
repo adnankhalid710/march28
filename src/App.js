f7b7ab589b509277084318ebab903576f22a9cf0
Muhammad Adnan Khalid <adnankhl@yahoo.com>
2024-03-30 08:23:46 +0800

    modify text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          In the name of Allah.
           Wednesday, March 28, 2024.Wednesday.
           
          
