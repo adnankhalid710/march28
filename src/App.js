b021a965ac5d91b21ae6eed802a74687ef6aa638
Muhammad Adnan Khalid <adnankhl@yahoo.com>
2024-03-30 08:17:14 +0800

    replace link

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ function App() {
           In the name of Allah.
           March 28, 2024.Wednesday.
           Have a nice day today.
-          <br>
-          <a href="https://www.researchgate.net/profile/Sadaf-Habib">Dr. Sadaf Habib</a>
-          </br>
+          
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Dr. Sadaf Habib"
+          href="https://www.researchgate.net/profile/Sadaf-Habib"
           target="_blank"
           rel="noopener noreferrer"
         >
